83b65abf560f2aa28fc7ef65ef1844961d316b36
Luis G. Villegas <luisguveal@gmail.com>
2022-03-04 21:47:05 -0430

    Setting up AWS credentials and client for uploading private files

--- server/services/upload.js
+++ server/services/upload.js
@@ -5,6 +5,7 @@
  */
 
 const Mux = require('@mux/mux-node')
+const AWS = require('aws-sdk');
 const pluginId = require("../pluginId")
 
 module.exports = {
@@ -12,13 +13,24 @@ module.exports = {
     mux_access_key_id: "",
     mux_access_key_secret: "",
     mux_signing_key_id: "",
-    mux_signing_private_key: ""
+    mux_signing_private_key: "",
+    aws_bucket: "",
+    aws_region: "",
+    aws_access_key_id: "",
+    aws_access_key_secret: ""
   },
   mux_client: null,
-  isValidConfig: function(config) {
+  aws_client: null,
+  isValidMuxConfig: function(config) {
     return (
       config.mux_access_key_id     !== "" &&
       config.mux_access_key_secret !== ""
+    )
+  },
+  isValidAWSConfig: function(config) {
+    return (
+      config.aws_access_key_id     !== "" &&
+      config.aws_access_key_secret !== ""
     )
   },
   /**
@@ -36,7 +48,11 @@ module.exports = {
     if (!config) {
       return this.DEFAULT_CONFIG
     }
-    return config
+    const fullconfig = {
+      ...this.DEFAULT_CONFIG,
+      ...config
+    }
+    return fullconfig
   },
   setConfig: async function(newConfigInput) {
     const config = await this.getConfig()
@@ -50,6 +66,13 @@ module.exports = {
     pluginStore.set({ key: "config", value: newConfig})
     this.setMuxClient(newConfig)
   },
+  getAWSClient: async function() {
+    if (!this.aws_client) {
+      const config = await this.getConfig()
+      this.setAWSClient(config)
+    }
+    return this.aws_client
+  },
   getMuxClient: async function() {
     if (!this.mux_client) {
       const config = await this.getConfig()
@@ -58,8 +81,18 @@ module.exports = {
     return this.mux_client
   },
   setMuxClient: function(config) {
-    if (this.isValidConfig(config)) {
+    if (this.isValidMuxConfig(config)) {
       this.mux_client = new Mux(config.mux_access_key_id, config.mux_access_key_secret)
+    }
+  },
+  setAWSClient: function(config) {
+    if (this.isValidAWSConfig(config)) {
+      AWS.config.update({region: config.aws_region})
+
+      this.aws_client = new AWS.S3({
+        accessKeyId: config.aws_access_key_id,
+        secretAccessKey: config.aws_access_key_secret
+      })
     }
   }
 }
