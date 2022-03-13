ced4dbeb33aee35076cc984d936562e460984b7f
Luis G. Villegas <luisguveal@gmail.com>
2022-03-12 19:38:00 -0430

    Filter ejercicios before assigning to user to avoid duplicate ejercicios

--- server/services/ejercicios.js
+++ server/services/ejercicios.js
@@ -2,7 +2,7 @@
 
 module.exports = ({ strapi }) => ({
   async assignEjercicios(user, ejercicios) {
-    let student = await strapi.db.query("plugin::masterclass.mc-student").findOne({
+    const student = await strapi.db.query("plugin::masterclass.mc-student").findOne({
       where: {user: user.id},
       populate: {
         ejercicios: {
@@ -11,18 +11,22 @@ module.exports = ({ strapi }) => ({
       }
     })
     if (!student) {
-      student = await strapi.entityService.create("plugin::masterclass.mc-student", {
+      await strapi.entityService.create("plugin::masterclass.mc-student", {
         data: {
           user: user.id,
           ejercicios: ejercicios.map(e => e.id)
         }
       })
     } else {
+      const newEjercicios = ejercicios.filter(e => {
+        const alreadyAssigned = student.ejercicios.some(({id}) => id === e.id)
+        return !alreadyAssigned
+      })
       await strapi.entityService.update("plugin::masterclass.mc-student", student.id, {
         data: {
           ejercicios: [
             ...student.ejercicios.map(e => e.id),
-            ...ejercicios.map(e => e.id)
+            ...newEjercicios.map(e => e.id)
           ]
         }
       })
