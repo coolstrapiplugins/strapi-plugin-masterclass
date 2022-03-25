3db441d47325052aedbf3dacb106c2014defcea5
Luis G. Villegas <luisguveal@gmail.com>
2022-03-24 21:21:20 -0430

    Setting up controllers for list-courses, create and update

--- server/controllers/admin__courses.js
+++ server/controllers/admin__courses.js
@@ -21,10 +21,21 @@ module.exports = {
                 select: ["id", "username", "email"]
               }
             }
+          },
+          category: {
+            select: ["id", "title", "slug"]
+          },
+          featured_in: {
+            select: ["id", "title", "slug"]
           }
         }
       }
     )
+    courses.forEach(course => {
+      const lecturesOrdered = strapi.service("plugin::masterclass.courses").orderLectures(course)
+
+      course.lectures = lecturesOrdered
+    })
     ctx.body = { courses }
   },
   async linkLectures(ctx) {
@@ -65,5 +76,91 @@ module.exports = {
       }
     })
     return {ok: true}
+  },
+  async listCourses(ctx) {
+    const courses = await strapi.entityService.findMany("plugin::masterclass.mc-course", {
+      filters: {},
+      fields: ["title", "slug"]
+    })
+    ctx.body = {
+      courses
+    }
+  },
+  async create(ctx) {
+    // ctx.request.body -> { title, slug, price, description, long_description }
+    const course = await strapi.entityService.create("plugin::masterclass.mc-course", {
+      data: ctx.request.body,
+      populate: {
+        lectures: {
+          fields: ["id","title"]
+        },
+        category: {
+          fields: ["id","slug","title"]
+        },
+        featured_in: {
+          fields: ["id","slug","title"]
+        },
+        students: {
+          fields: []
+        }
+      }
+    })
+    ctx.body = { course }
+  },
+  async update(ctx) {
+    const { id } = ctx.params
+    // ctx.request.body
+    /* ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
+      {
+        title,
+        slug,
+        price,
+        description,
+        long_description,
+        lectures,
+        category,
+        featured_in
+      }
+    */
+    const { lectures } = ctx.request.body
+
+    const storedLectures = await strapi.entityService.findMany("plugin::masterclass.mc-lecture", {
+      filters: {
+        id: lectures
+      },
+      populate: {
+        video: {
+          fields: ["duration"]
+        }
+      }
+    })
+    const duration = storedLectures.reduce((totalDuration, lecture) => {
+      if (lecture.video) {
+        totalDuration += lecture.video.duration
+      }
+      return totalDuration
+    }, 0)
+    const course = await strapi.entityService.update("plugin::masterclass.mc-course", id, {
+      data: {
+        ...ctx.request.body,
+        lectures_order: lectures,
+        duration
+      },
+      populate: {
+        lectures: {
+          fields: ["id","title"]
+        },
+        category: {
+          fields: ["id","slug","title"]
+        },
+        featured_in: {
+          fields: ["id","slug","title"]
+        },
+        students: {
+          fields: []
+        }
+      }
+    })
+    ctx.body = { course }
   }
 }
