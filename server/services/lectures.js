51e89f54020b1927310ba0e0eabf50ab87d2a38f
Luis G. Villegas <luisguveal@gmail.com>
2022-03-24 21:21:22 -0430

    Setting up service to upload video to Mux

--- server/services/lectures.js
+++ server/services/lectures.js
@@ -1,11 +1,15 @@
 'use strict';
 
+const fs = require('fs');
+const request = require('request');
+const uuid = require('uuid');
+
 /**
  *  service.
  */
 
 module.exports = ({ strapi }) => ({
-  /*
+  /**
    *  Create video and link to lecture
    */
   async storeLecture(data) {
@@ -13,7 +17,8 @@ module.exports = ({ strapi }) => ({
     const newVideoData = {
       video_id: data.playback_ids[0].id,
       filename: data.id,
-      duration: parseInt(data.duration)
+      duration: parseInt(data.duration),
+      ready: true
     }
     const newVideo = await strapi.entityService.create("plugin::masterclass.mc-video",
       { data: newVideoData }
@@ -27,8 +32,57 @@ module.exports = ({ strapi }) => ({
       { data: newLectureData }
     )
     return {
+      id: newLecture.id,
       title: data.id,
       video: newVideoData
     }
+  },
+  /**
+   * Upload video to Mux
+   */
+  async uploadVideo(video) {
+    // Get mux client
+    const muxClient = await strapi.service('plugin::masterclass.upload').getMuxClient()
+    const config = await strapi.service('plugin::masterclass.upload').getConfig()
+    if (!muxClient) {
+      throw {
+        status: "badRequest",
+        msg: "Config is not valid",
+        data: config
+      }
+    }
+
+    const tempID = uuid.v1()
+
+    let upload
+    try {
+      const { Video } = muxClient
+      // upload.url is where the video will be put.
+      upload = await Video.Uploads.create({
+        new_asset_settings: {
+          passthrough: tempID,
+          playback_policy: 'signed'
+        }
+      })
+    } catch(err) {
+      console.log(err)
+      throw {
+        status: "internalServerError",
+        msg: "Error on Video.Uploads.create"
+      }
+    }
+
+    try {
+      // Uplaod video to bucket
+      await fs.createReadStream(video.path).pipe(request.put(upload.url))
+    } catch(err) {
+      console.log(err)
+      throw {
+        status: "internalServerError",
+        msg: "Error on request.put"
+      }
+    }
+    // Upload finished
+    return tempID
   }
 })
